218e399d0e74bbc00d609fbc16cf08b0ab9313fa
Divyangkumar87 <63184784+Divyangkumar87@users.noreply.github.com>
2026-08-07 07:53:28 +0530

    Add: top-k-elements-in-list - submission-9

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-9.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        
+        Queue<int[]> heap = new PriorityQueue<>((a, b) -> Integer.compare(a[1], b[1]));
+        Map<Integer, int[]> map = new HashMap<>();
+        int[] ans = new int[k];
+
+        for(int i = 0; i < nums.length; i++) {
+            if(map.containsKey(nums[i])) {
+                int[] arr = map.get(nums[i]);
+                arr[1]++;
+            } else {
+                map.put(nums[i], new int[]{nums[i], 1});
+            }
+        }
+        
+        for(Integer key : map.keySet()) {
+            heap.offer(map.get(key));
+            if(heap.size() > k) {
+                heap.poll();
+            }
+        }
+
+        for(int i = 0; i < k; i++) {
+            ans[i] = heap.poll()[0];
+        }
+
+        return ans;
+    }
+}
